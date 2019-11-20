362946bd4199bba144767a379f29f8c120225bbd
MrChuse <aleksha007@mail.ru>
2019-11-20 20:05:31 +0300

    dense bias chance
    bias = 0
    small Dense correction and tests after math commit

--- Dense.py
+++ Dense.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 class Dense:
-    x = None
 
     def __init__(self, num1, num2, learning_rate=0.01, bias=True):
         self.mt = 2/num1 * np.random.randn(num1, num2) # new weights according to Andrew Ng
-        self.bias = 2/num1 * np.random.randn(1, num2)
+        self.bias = np.random.zeros(1, num2)
         self.lr = learning_rate
 
     def forward(self, data, bias=True, prnt=False):
@@ -23,8 +22,3 @@ class Dense:
         if bias:
             self.bias -= dz * self.lr
         return dx
-
-if __name__ == '__main__':
-    b = Dense(3, 5)
-    x = b.forward(np.array([0, 0, 1]))
-    print(x)
